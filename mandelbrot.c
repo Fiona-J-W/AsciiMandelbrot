f219d40ecedc3f46480a5aa326a0db623b027338
Mekeor Melire <mekeor@ymail.com>
2011-11-26 11:15:36 +0100

    added optional accuracy parameter

--- mandelbrot.c
+++ mandelbrot.c
@@ -2,25 +2,38 @@
 #include <stdlib.h>
 
 
-int condition (double x, double y) {
+
+#define ACCURACY 9999999
+
+
+int condition (double x, double y, unsigned a) {
+	
 	unsigned counter=1;
-	double imag=0, real=0;
-	while (counter<=100) {
+	double   imag=0,
+		 real=0;
+	
+	while (counter<=a) {
 		real=real*real-imag*imag+x;
 		imag=2*real*imag+y;
 		if (real*real+imag*imag>4) break;
 		counter++;
 	}
-	if (counter>100) return 1;
-	else             return 0;
+	
+	if (counter>a) return 1;
+	else           return 0;
 }
+
 
 int main (int argc, char **argv) {
 	
-	if (argc!=3) {
-		printf("Usage: %s width height\n", argv[1]);
+	if (argc<3 || argc>4) {
+		printf("Usage: %s width height [accuracy]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+	
+	unsigned a; // accuracy
+	if (argc==4) a=atoi(argv[3]);
+	else         a=1000;
 	
 	double we=atoi(argv[1]),
 	       he=atoi(argv[2]),
@@ -29,8 +42,8 @@ int main (int argc, char **argv) {
 	
 	for (double y=-1; y<=1; y+=h) {
 		for (double x=-2; x<=1; x+=w)
-			if (condition(x,y)) putchar('#');
-			else                putchar(' ');
+			if (condition(x,y,a)) putchar('#');
+			else                  putchar(' ');
 		putchar('\n');
 	}
 	
