11e7e443ce23f5bd5a339ab716b26ec4fe4c409a
Mekeor Melire <mekeor@ymail.com>
2011-11-27 10:39:12 +0100

    added color support

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,36 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int colors[]={ 7,  //white
+	       3,  //yellow
+	       2,  //green
+	       6,  //cyan
+	       4,  //blue
+	       5,  //magenta
+	       1,  //red
+	       0}; //black
 
 
-int condition (double x, double y, unsigned a) {
+
+void f (double x, double y, unsigned d, unsigned a) {
 	
 	unsigned counter=1;
 	double   imag=0,
 		 real=0;
 	
-	while (counter<=a) {
+	while (counter<=d) {
 		real=real*real-imag*imag+x;
 		imag=2*real*imag+y;
-		if (real*real+imag*imag>4) break;
+		if (real*real+imag*imag>a) break;
 		counter++;
 	}
 	
-	if (counter>a) return 1;
-	else           return 0;
+	for (unsigned u=0; u<=7; ++u)
+		if (counter>d-u*d/7) {
+			printf("\033[4%dm",colors[u]);
+			break;
+		}
+	putchar(' ');
 }
 
 
 int main (int argc, char **argv) {
 	
-	if (argc<3 || argc>4) {
-		printf("Usage: %s width height [accuracy]\n", argv[0]);
+	if (argc<3 || argc>5) {
+		printf("Usage: %s width height [depth [accuracy]]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	
+	unsigned d; // depth
+	if (argc>3) d=atoi(argv[3]);
+	else        d=1000;
+	
 	unsigned a; // accuracy
-	if (argc==4) a=atoi(argv[3]);
-	else         a=1000;
+	if (argc>4) a=atoi(argv[4]);
+	else        a=4;
 	
 	double we=atoi(argv[1]),
 	       he=atoi(argv[2]),
@@ -39,9 +56,8 @@ int main (int argc, char **argv) {
 	
 	for (double y=-1; y<=1; y+=h) {
 		for (double x=-2; x<=1; x+=w)
-			if (condition(x,y,a)) putchar('#');
-			else                  putchar(' ');
-		putchar('\n');
+			f(x,y,d,a);
+		printf("\033[0m\n");
 	}
 	
 	return EXIT_SUCCESS;
